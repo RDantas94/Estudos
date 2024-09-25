814717fa639e890b4a0ec006f623e4ea93d85061
Rafael Dantas Menezes <rdmenezes94@hotmail.com>
2024-09-25 14:25:04 -0300

    realizar desafio alugames
    criação de um programa para aluguel e devolução de board games.

---
+++ alugames/js/app.js
@@ -0,0 +1,15 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+
+    if (botao.textContent === "Alugar") {
+        botao.textContent = "Devolver";
+        botao.classList.add('dashboard__item__button--return');
+        imagem.classList.add('dashboard__item__img--rented');
+    } else {
+        botao.textContent = "Alugar";
+        botao.classList.remove('dashboard__item__button--return');
+        imagem.classList.remove('dashboard__item__img--rented');
+    }
+}
